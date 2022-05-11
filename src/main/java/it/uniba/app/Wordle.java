c55525e34950fd494408fae1f4ff1e7cdc12e244
Emanuele Tanzi <e.tanzi2@studenti.uniba.it>
2022-05-11 17:46:40 +0200

    Creazione classe Wordle.

---
+++ src/main/java/it/uniba/app/Wordle.java
@@ -0,0 +1,16 @@
+package it.uniba.app;
+
+/** Gestisce la logica del gioco
+ *
+ */
+public class Wordle
+{
+    static String secretWord = null;
+    static WordleGame currentGame = null;
+
+    //startGame() { new WordleGame(secretWord, 6, 5) }
+    //guess()
+    //endGame()
+    //setSecretWord()
+    //showSecretWord()
+}
