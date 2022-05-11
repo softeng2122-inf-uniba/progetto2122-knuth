a610235b8f6866ea5c5e8a2b7c6ec3a6184044c0
Antonio Mari <grasarade@gmail.com>
2022-05-11 17:28:49 +0200

    Creazione classe interna LetterBox e attributi

--- src/main/java/it/uniba/app/Guess.java
+++ src/main/java/it/uniba/app/Guess.java
@@ -1,6 +1,17 @@
 package it.uniba.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Guess
 {
+    List<LetterBox> cellArray;
+
+    public class LetterBox
+    {
+        Character letter;
+        String color;
+    }
 
 }
+
