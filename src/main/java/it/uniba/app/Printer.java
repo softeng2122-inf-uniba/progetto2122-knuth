30eef2e94a8c41846acafa7f8cf26186ad043785
Gabriele Marrano <g.marrano2@studenti.uniba.it>
2022-05-11 20:39:17 +0200

    Creazione classe Printer

---
+++ src/main/java/it/uniba/app/Printer.java
@@ -0,0 +1,11 @@
+package it.uniba.app;
+
+/** Questa classe si occupa delle stampe sul terminale
+ *
+ */
+public class Printer
+{
+    //static void printBoard()
+    //static void printSecretWord()
+    //static void printHelp()
+}
