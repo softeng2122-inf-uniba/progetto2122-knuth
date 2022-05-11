0f3913817ad76df2447cea814560514a60d929ab
Antonio Mari <grasarade@gmail.com>
2022-05-11 17:30:13 +0200

    Creazione commenti Javadoc

--- src/main/java/it/uniba/app/Guess.java
+++ src/main/java/it/uniba/app/Guess.java
@@ -3,10 +3,16 @@ package it.uniba.app;
 import java.util.ArrayList;
 import java.util.List;
 
+/** Rappresenta un tentativo del giocatore che cerca di indovinare la parola segreta
+ *
+ */
 public class Guess
 {
     List<LetterBox> cellArray;
 
+    /** Rappresenta una cella contenente una lettera del tentativo e il colore associato
+     *
+     */
     public class LetterBox
     {
         Character letter;
