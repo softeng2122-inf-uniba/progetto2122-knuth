3a6ec250fcd886bcd8ac0fceefa6bcfce3e159c4
Antonio Mari <grasarade@gmail.com>
2022-05-12 09:54:56 +0200

    Aggiunte costanti di caratteri unicode per la stampa della Board

--- src/main/java/it/uniba/app/Printer.java
+++ src/main/java/it/uniba/app/Printer.java
@@ -5,7 +5,33 @@ package it.uniba.app;
  */
 public class Printer
 {
-    //static void printBoard()
+
+    // angoli (L = left, R = right, U = up, D = down)
+    private static final char L_U_ANGLE = '\u2554';
+    private static final char R_U_ANGLE = '\u2557';
+    private static final char L_D_ANGLE = '\u255A';
+    private static final char R_D_ANGLE = '\u255D';
+
+    // separatori
+    private static final char L_SEPARATOR = '\u2560';
+    private static final char R_SEPARATOR = '\u2563';
+    private static final char U_SEPARATOR= '\u2566';
+    private static final char D_SEPARATOR= '\u2569';
+    private static final char CROSS = '\u256c';
+
+    // spigoli
+    private static final char VERTICAL_EDGE = '\u2551';
+    private static final char HORIZONTAL_EDGE = '\u2550';
+
+    // unioni
+    private static final String HORIZONTAL_EDGE_X3 = HORIZONTAL_EDGE + "" + HORIZONTAL_EDGE + HORIZONTAL_EDGE;
+
+
+    static void printBoard()
+    {
+
+    }
+
     //static void printSecretWord()
     //static void printHelp()
 }
