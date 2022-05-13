2ca1ab7d5e970f58335fd0805d3455f9a83c98ee
Gabriele Marrano <g.marrano2@studenti.uniba.it>
2022-05-13 18:36:05 +0200

    Aggiunti attributi d'istanza e costruttore della classe Parser

--- src/main/java/it/uniba/app/Parser.java
+++ src/main/java/it/uniba/app/Parser.java
@@ -5,6 +5,20 @@ package it.uniba.app;
  */
 public class Parser
 {
+    private String input;
+    private Command command;
+    private String[] args;
+    public Parser() {
+        this.input = null;
+        this.command = null;
+        this.args = null;
+    }
+    /*
+    public Parser(String inputLine)
+    {
+        feed(inputLine);
+    }
+    */
     public static Command parseInput(String inputLine)
     {
         Command outParser;
