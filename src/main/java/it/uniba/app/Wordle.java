869b8e3cdc44811dc60ee2552032b53929afb80f
Antonio Mari <grasarade@gmail.com>
2022-05-14 12:34:06 +0200

    Aggiunta intestazione metodi guess e getGuessResult

--- src/main/java/it/uniba/app/Wordle.java
+++ src/main/java/it/uniba/app/Wordle.java
@@ -9,7 +9,25 @@ public class Wordle
     static WordleGame currentGame = null;
 
     //startGame()
-    //guess()
+    static void guess(String guessWord)
+    {
+        // controllo sulla partita che deve esistere
+        // controllo sulla parola
+        // cioè controllo su lunghezza e validità caratteri
+
+        // modifica della matrice dei tentativi
+
+        // se hai indovinato dillo, se era l'ultimo tentativo possibile dillo
+        // in questi casi chiudi la partita
+    }
+
+    static int getGuessResult()
+    {
+        // ritorna 1 se il tentativo è vincente
+        // ritorna 0 se il tentativo non è vincente
+    }
+
+
     //endGame()
     //setSecretWord()
     //showSecretWord()
