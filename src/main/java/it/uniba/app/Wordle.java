d2fd4b88d0f74f852ae6ec0991ee9bbc7d1b7c3e
Antonio Mari <grasarade@gmail.com>
2022-05-14 19:48:39 +0200

    Creato nucleo del metodo guess.

--- src/main/java/it/uniba/app/Wordle.java
+++ src/main/java/it/uniba/app/Wordle.java
@@ -1,5 +1,8 @@
 package it.uniba.app;
 
+import java.util.Map;
+import java.util.HashMap;
+
 /** Gestisce la logica del gioco
  *
  */
@@ -25,8 +28,50 @@ public class Wordle
         // controllo sulla parola
         // cioè controllo su lunghezza e validità caratteri
 
-        // modifica della matrice dei tentativi
+        String secretWord = currentGame.getSecretWord();
+        Guess newGuess = new Guess(guessWord);
 
+        // dizionario per contenere coppie (lettera, numOccorrenze) della secret
+        Map<Character, Integer> letterMap = new HashMap<>();
+
+        // metti nel dizionario le coppie
+        for(char letter : secretWord.toCharArray())
+        {
+            if(!letterMap.containsKey(letter))
+                letterMap.put(letter, 1);
+            else
+                letterMap.put(letter, letterMap.get(letter) + 1);
+        }
+
+        //primo ciclo: controlla se è verde
+        for(int i = 0; i < guessWord.length(); i++)
+        {
+            char l = guessWord.charAt(i);
+            if(l == secretWord.charAt(i))
+            {
+                newGuess.setColor(i, Color.GREEN);
+
+                //decrementa dizionario
+                letterMap.put(l, letterMap.get(l) - 1);
+            }
+        }
+
+        // secondo ciclo: controlla se è giallo
+        for(int i = 0; i < guessWord.length(); i++)
+        {
+            char l = guessWord.charAt(i);
+            if(letterMap.containsKey(l) && letterMap.get(l) > 0) //test YELLOW
+            {
+                newGuess.setColor(i, Color.YELLOW);
+                letterMap.put(l, letterMap.get(l) - 1);
+            }
+            else
+                newGuess.setColor(i, Color.GREY);
+        }
+
+        // aggiungi il tentativo alla matrice dei tentativi
+        Board gameBoard = currentGame.getGameBoard();
+        gameBoard.acceptNewGuess(newGuess);
         // se hai indovinato dillo, se era l'ultimo tentativo possibile dillo
         // in questi casi chiudi la partita
     }
