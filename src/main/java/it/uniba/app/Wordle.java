c32f84012d8b383e060ebbc915f15b3a3909d789
Emanuele Tanzi <e.tanzi2@studenti.uniba.it>
2022-05-14 17:13:48 +0200

    Realizzazione metodo setSecretWord

--- src/main/java/it/uniba/app/Wordle.java
+++ src/main/java/it/uniba/app/Wordle.java
@@ -20,6 +20,22 @@ public class Wordle
     //guess()
     //endGame()
     //setSecretWord()
+    public void setSecretWord(String secretWord, int wordLength) throws Exception
+    {
+        // controllo sulla dimensione
+        if (secretWord.length() < wordLength)
+        {
+            throw new Exception("Parola troppo corta");
+        }
+        // controllo sui caratteri
+        if (!secretWord.matches("[a-zA-Z]+"))
+        {
+            throw new Exception("Parola contenente caratteri diversi da lettere");
+        }
+
+        this.secretWord = secretWord;
+    }
+
     //showSecretWord()
 
     public static boolean isGameRunning()
