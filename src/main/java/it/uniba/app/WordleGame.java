3cc76e7461e73237b0a7420eb47dc2f71f198844
npinto97 <ningo1797@gmail.com>
2022-05-15 11:37:06 +0200

    Eliminazione di getter, setter ridondanti (WordleGame).

--- src/main/java/it/uniba/app/WordleGame.java
+++ src/main/java/it/uniba/app/WordleGame.java
@@ -21,26 +21,6 @@ public class WordleGame
         this.gameBoard = new Board(column, row);
     }
 
-    public Color getColor(int row, int column)
-    {
-        return gameBoard.getColor(row, column);
-    }
-
-    public void setColor(int row, int column, Color color)
-    {
-        gameBoard.setColor(row, column, color);
-    }
-
-    public char getLetter(int row, int column)
-    {
-        return gameBoard.getLetter(row, column);
-    }
-
-    public void setLetter(int row, int column, char letter)
-    {
-        gameBoard.setLetter(row, column, letter);
-    }
-
     public String getSecretWord()
     {
         return secretWord;
