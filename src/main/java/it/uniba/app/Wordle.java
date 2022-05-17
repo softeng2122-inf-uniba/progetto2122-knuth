3fb0d3f257bd3e2b44531842580884b58d750a65
Emanuele Tanzi <e.tanzi2@studenti.uniba.it>
2022-05-17 13:21:02 +0200

    Modificate le stampe di avviso

--- src/main/java/it/uniba/app/Wordle.java
+++ src/main/java/it/uniba/app/Wordle.java
@@ -157,16 +157,16 @@ public class Wordle
     {
         if (newWord.length() < wordLength)
         {
-            throw new Exception("Parola troppo corta");
+            throw new Exception("Parola segreta troppo corta");
         }
         if (newWord.length() > wordLength)
         {
-            throw new Exception("Parola troppo lunga");
+            throw new Exception("Parola segreta troppo lunga");
         }
 
         if (!newWord.matches("[a-zA-Z]+"))
         {
-            throw new Exception("Parola contenente caratteri diversi da lettere");
+            throw new Exception("Parola segreta non valida");
         }
 
         if (isGameRunning())
