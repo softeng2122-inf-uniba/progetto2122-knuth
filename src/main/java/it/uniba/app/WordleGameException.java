bb726b82346cc7c1b9c2d5ca5acf0c58453c57e2
npinto97 <ningo1797@gmail.com>
2022-05-18 19:35:49 +0200

    Aggunti commenti javadoc (WordleGameException, WordleSettingException).

--- src/main/java/it/uniba/app/WordleGameException.java
+++ src/main/java/it/uniba/app/WordleGameException.java
@@ -1,5 +1,12 @@
 package it.uniba.app;
 
+/**
+ * {@literal <<NoECB>>} <br>
+ *  Eccezione lanciata in situazioni che richiedono come precondizione l'esistenza o la non esistenza della partita,
+ *  nelle quali essa non è rispettata.
+ *
+ *  Esempio: Precondizione per effettuare un tentativo è l'aver iniziato una partita.
+ */
 public class WordleGameException extends RuntimeException
 {
     WordleGameException (String message)
--- src/main/java/it/uniba/app/WordleSettingException.java
+++ src/main/java/it/uniba/app/WordleSettingException.java
@@ -1,5 +1,12 @@
 package it.uniba.app;
 
+/**
+ * {@literal <<NoECB>>} <br>
+ *  Eccezione lanciata in situazioni che richiedono particolari precondizioni sulle impostazioni di gioco,
+ *  nelle quali esse non sono rispettate.<p></p>
+ *
+ *  Esempio: Precondizione per iniziare una partita è l'aver impostato una parola segreta.
+ */
 public class WordleSettingException extends RuntimeException
 {
     WordleSettingException (String message)
