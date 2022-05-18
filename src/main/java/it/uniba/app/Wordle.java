fa82a79dd78fc044a675ea721e05c3fe98992ec3
npinto97 <ningo1797@gmail.com>
2022-05-18 20:54:02 +0200

    Aggunti commenti javadoc (Wordle).

--- src/main/java/it/uniba/app/Wordle.java
+++ src/main/java/it/uniba/app/Wordle.java
@@ -4,8 +4,12 @@ package it.uniba.app;
 import java.util.Map;
 import java.util.HashMap;
 
-/** Gestisce la logica del gioco
- *
+/**
+ * {@literal <<Control>>} <br>
+ * Classe che implementa la logica del gioco: opera su oggetti delle classi di tipo Entity,
+ * coerentemente con le regole di Wordle. <p></p>
+ * Fornisce una API (Application Programming Interface) indipendente dallo strato di User Interface,
+ * il quale richiamerà i suoi servizi, effettuando controlli sulla legalità delle chiamate.
  */
 public class Wordle
 {
@@ -14,6 +18,12 @@ public class Wordle
     private static int nMaxGuesses = 6;
     private static int wordLength = 5;
 
+    /**
+     * Inizia una nuova partita a Wordle.
+     *
+     * @throws WordleGameException  se una partita è già in corso
+     * @throws WordleSettingException   se la parola segreta non è impostata
+     */
     public static void startGame() throws WordleGameException, WordleSettingException
     {
         if (isGameRunning())
@@ -25,6 +35,17 @@ public class Wordle
         currentGame = new WordleGame(secretWord);
     }
 
+    /**
+     * Effettua un tentativo e lo inserisce nella matrice dei tentativi della partita corrente.<br>
+     * Per ogni lettera della parola inserita colora lo sfondo (della casella corrispondente)
+     * di verde se la lettera è nella parola segreta e nel posto giusto,
+     * di giallo se la lettera è nella parola segreta ma nel posto sbagliato e
+     * di grigio se la lettera non è nella parola segreta.
+     *
+     * @param guessWord stringa contenente la parola inserita dal giocatore
+     * @throws WordleGameException  se nessuna partita è in corso o se è stato già raggiunto il numero massimo di tentativi
+     * @throws IllegalArgumentException   se {@code guessWord} non soddisfa la lunghezza prevista o contiene caratteri non validi
+     */
     public static void guess(String guessWord) throws WordleGameException, IllegalArgumentException
     {
         if (!isGameRunning())
@@ -92,7 +113,12 @@ public class Wordle
         gameBoard.acceptNewGuess(newGuess);
     }
 
-    // per convenzione, se non hai fatto ancora tentativi return false
+    /**
+     * Restituisce il risultato dell'ultimo tentativo effettuato.
+     * @throws WordleGameException  se nessuna partita è in corso
+     * @return true se il tentativo è vincente, false se non è vincente
+     * oppure se non sono stati effettuati tentativi nella partita corrente
+     */
     public static boolean getGuessResult()
     {
         if(!isGameRunning())
@@ -110,7 +136,10 @@ public class Wordle
         }
     }
 
-    // restituisci il numero di tentativi che rimangono
+    /**
+     * Restituisce il numero di tentativi rimanenti per la partita corrente.
+     * @throws WordleGameException  se nessuna partita è in corso
+     */
     public static int getNumRemainingGuesses()
     {
         if(!isGameRunning())
@@ -119,7 +148,10 @@ public class Wordle
         return currentGame.getNumRemainingGuesses();
     }
 
-
+    /**
+     * Restituisce il numero totale di tentativi per la partita corrente.
+     * @throws WordleGameException  se nessuna partita è in corso
+     */
     public static int getMaxGuesses()
     {
         if(!isGameRunning())
@@ -128,6 +160,10 @@ public class Wordle
         return currentGame.getMaxGuesses();
     }
 
+    /**
+     * Restituisce la lunghezza della parola segreta.
+     * @throws WordleGameException  se nessuna partita è in corso
+     */
     public static int getWordLength()
     {
         if(!isGameRunning())
@@ -136,6 +172,18 @@ public class Wordle
         return currentGame.getWordLength();
     }
 
+    /**
+     * Restituisce la lettera contenuta in una casella della matrice dei tentativi della partita corrente.
+     * @param row   il valore 0 indica la prima riga, il valore i-esimo indica la riga i+1,
+     *              il valore massimo corrisponde al numero di righe della matrice dei tentativi - 1
+     * @param column    il valore 0 indica la prima colonna, il valore i-esimo indica la colonna i+1,
+     *                  il valore massimo corrisponde al numero di colonne della matrice dei tentativi - 1
+     * @return  la lettera nella casella alla riga {@code row} e alla colonna {@code column} se il tentativo
+     * corrispondente alla riga {@code row} è stato effettuato, ' ' altrimenti
+     * @throws WordleGameException   se nessuna partita è in corso
+     * @throws  IllegalArgumentException se {@code row} o {@code column} eccedono le dimensioni della matrice dei tentativi
+     * oppure sono numeri negativi
+     */
     public static char getLetter(int row, int column)
     {
         if(!isGameRunning())
@@ -151,6 +199,18 @@ public class Wordle
             return currentBoard.getGuess(row).getLetter(column);
     }
 
+    /**
+     * Restituisce il colore di una casella della matrice dei tentativi della partita corrente.
+     * @param row   il valore 0 indica la prima riga, il valore i-esimo indica la riga i+1,
+     *              il valore massimo corrisponde al numero di righe della matrice dei tentativi - 1
+     * @param column    il valore 0 indica la prima colonna, il valore i-esimo indica la colonna i+1,
+     *                  il valore massimo corrisponde al numero di colonne della matrice dei tentativi - 1
+     * @return  il colore della casella alla riga {@code row} e alla colonna {@code column} se il tentativo
+     * corrispondente alla riga {@code row} è stato effettuato, {@code NO_COLOR} altrimenti
+     * @throws WordleGameException   se nessuna partita è in corso
+     * @throws  IllegalArgumentException se {@code row} o {@code column} eccedono le dimensioni della matrice dei tentativi
+     * oppure sono numeri negativi
+     */
     public static Color getColor(int row, int column)
     {
         if(!isGameRunning())
@@ -166,6 +226,10 @@ public class Wordle
             return currentBoard.getGuess(row).getColor(column);
     }
 
+    /**
+     * Termina la partita in corso.
+     * @throws WordleGameException se nessuna partita è in corso
+     */
     public static void endGame() throws WordleGameException
     {
         if(!isGameRunning())
@@ -174,7 +238,12 @@ public class Wordle
         currentGame = null;
     }
 
-    //setSecretWord()
+    /**
+     * Imposta la parola segreta.
+     * @param newWord   parola segreta da impostare
+     * @throws IllegalArgumentException se {@code newWord} non soddisfa la lunghezza prevista o contiene caratteri non validi
+     * @throws WordleGameException  se una partita è già in corso
+     */
     public static void setSecretWord(String newWord) throws IllegalArgumentException, WordleGameException
     {
         if (!newWord.matches("[a-zA-Z]+"))
@@ -192,6 +261,9 @@ public class Wordle
         secretWord = newWord.toUpperCase();
     }
 
+    /**
+     * @return true se è in corso una partita, false altrimenti
+     */
     public static boolean isGameRunning()
     {
         return currentGame != null;
@@ -209,6 +281,15 @@ public class Wordle
             throw new IllegalArgumentException("Tentativo eccessivo");
     }
 
+    /**
+     * Restituisce la parola segreta dalle impostazioni di gioco.<p></p>
+     * Nota: questo metodo, molto simile a {@link Wordle#getGameSecretWord()}, si differenzia da esso per
+     * l'origine del dato restituito (la differenza risiede nel recuperare l'impostazione attuale sulla
+     * parola segreta piuttosto che recuperare la parola segreta della partita già iniziata).<br>
+     * La scelta di tenere separati i due metodi è stata presa in quanto il nostro team ritiene che favorisca
+     * la modificabilità del codice.
+     * @throws WordleSettingException  se la parola segreta non è stata impostata
+     */
     public static String getSecretWord() throws WordleSettingException
     {
         if (secretWord == null)
@@ -217,7 +298,10 @@ public class Wordle
         return secretWord;
     }
 
-    //nota: diversa dalla precedente in quanto ritorna la parola della partita in esecuzione
+    /**
+     * Restituisce la parola segreta per la partita corrente.
+     * @throws WordleGameException  se nessuna partita è in corso
+     */
     public static String getGameSecretWord() throws WordleGameException
     {
         if (!isGameRunning())
