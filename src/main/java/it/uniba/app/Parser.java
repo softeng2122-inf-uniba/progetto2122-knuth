7873c97bfbaa19a611e77c04355d98d8b442ad7d
kozen88 <93886188+kozen88@users.noreply.github.com>
2022-05-28 17:31:30 +0200

    Primo fix in conformità con checkstyle. (Parser)

--- src/main/java/it/uniba/app/Parser.java
+++ src/main/java/it/uniba/app/Parser.java
@@ -5,30 +5,26 @@ package it.uniba.app;
  * Classe per il parsing dell'input dell'utente. <p></p>
  * Estrae {@link Command} e relativi argomenti a partire dalle linee digitate.
  */
-public class Parser
-{
+public class Parser {
     private String input;
     private Command command;
     private String[] args;
     private String[] tokens;
 
-    public Parser()
-    {
+    public Parser() {
         this.input = null;
         this.command = null;
         this.args = null;
     }
 
-    public void feed(String inputLine)
-    {
+    public void feed(String inputLine) {
         this.input = inputLine.trim();
         tokens = tokenizeInput();
         this.command = extractCommand();
         this.args = extractArgs();
     }
 
-    private String[] tokenizeInput()
-    {
+    private String[] tokenizeInput() {
         if (input.length() == 0)
             return null;
 
@@ -37,21 +33,18 @@ public class Parser
         return tokens;
     }
 
-    private Command extractCommand()
-    {
+    private Command extractCommand() {
         Command command = Command.INVALID;
 
         // CASO 1: solo spazi
-        if (tokens == null)
-        {
+        if (tokens == null) {
             command = Command.SPACE;
             return command;
         }
 
         // CASO 2: comando effettivo
         char firstChar = tokens[0].charAt(0);
-        if (firstChar == '/')
-        {
+        if (firstChar == '/') {
             //salvo solo il comando, i valori di enum non contengono "/"
             String tokenCommand = tokens[0].substring(1);
 
@@ -72,26 +65,22 @@ public class Parser
 
             if (tokenCommand.equalsIgnoreCase(Command.HELP.toString()))
                 command = Command.HELP;
-        }
-        else //CASO 3: tentativo
+        } else {//CASO 3: tentativo
             command = Command.GUESS;
-
+        }
         return command;
     }
 
-    private String[] extractArgs()
-    {
+    private String[] extractArgs() {
         if (tokens == null)
             return null;
 
         //se il primo token è un comando lo salti
-        if (tokens[0].charAt(0) == '/')
-        {
+        if (tokens[0].charAt(0) == '/') {
             int numberArgsExpected;
 
             // imposta il numero di argomenti atteso
-            switch (command)
-            {
+            switch (command) {
                 case INVALID:
                 case GIOCA:
                 case ABBANDONA:
@@ -107,47 +96,39 @@ public class Parser
                     numberArgsExpected = 0;
             }
 
-            if (numberArgsExpected == 0)
+            if (numberArgsExpected == 0) {
                 return null;
-            else
-            {
-                // array di grandezza pari al numero massimo di argomenti del comando
+            } else {
+                // array di grandezza pari al numero massimo di argomenti
+                // del comando
                 String[] tempArgs = new String[numberArgsExpected];
                 int countArg = 0;
 
-                try
-                {
-                    //partendo dal token 1 (cioè escludendo quello contenente il comando)
-                    // inserisci in tempArgs
-                    for (int nToken = 1; nToken <= numberArgsExpected; nToken++)
-                    {
+                try {
+                    //partendo dal token 1 (cioè escludendo quello contenente
+                    // il comando) inserisci in tempArgs
+                    for (int nToken = 1; nToken <= numberArgsExpected; nToken++) {
                         tempArgs[countArg] = tokens[nToken];
                         countArg++;
                     }
                     return tempArgs;
-                } // nel caso in cui leggi un numero minore di argomenti
-                catch (ArrayIndexOutOfBoundsException e)
-                {
+                    // nel caso in cui leggi un numero minore di argomenti
+                } catch (ArrayIndexOutOfBoundsException e) {
                     return tempArgs;
                 }
             }
-        }
-        else //se è un tentativo considero solo il primo argomento
-        {
+        } else { //se è un tentativo considero solo il primo argomento
             String[] tempArgs = new String[1];
             tempArgs[0] = tokens[0];
             return tempArgs;
         }
     }
 
-    public Command getCommand()
-    {
+    public Command getCommand() {
         return this.command;
     }
 
-    public String[] getArgs()
-    {
+    public String[] getArgs() {
         return this.args;
     }
-
 }
