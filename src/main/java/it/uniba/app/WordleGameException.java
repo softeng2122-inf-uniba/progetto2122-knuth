48f938ee1963a1842faf9eaa7d80861f8f941b8a
kozen88 <93886188+kozen88@users.noreply.github.com>
2022-05-28 17:57:23 +0200

    Primo fix in conformità con checkstyle. (WordleGameException)

--- src/main/java/it/uniba/app/WordleGameException.java
+++ src/main/java/it/uniba/app/WordleGameException.java
@@ -2,15 +2,15 @@ package it.uniba.app;
 
 /**
  * {@literal <<NoECB>>} <br>
- *  Eccezione lanciata in situazioni che richiedono come precondizione l'esistenza o la non esistenza della partita,
- *  nelle quali essa non è rispettata.
+ * Eccezione lanciata in situazioni che richiedono come precondizione
+ * l'esistenza o la non esistenza della partita, nelle quali essa non
+ * è rispettata.
  *
- *  Esempio: Precondizione per effettuare un tentativo è l'aver iniziato una partita.
+ * Esempio: Precondizione per effettuare un tentativo è l'aver
+ * iniziato una partita.
  */
-public class WordleGameException extends RuntimeException
-{
-    WordleGameException (String message)
-    {
+public class WordleGameException extends RuntimeException {
+    WordleGameException(String message) {
         super(message);
     }
 }
