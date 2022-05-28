5ea8f9849b2a0acf8028e46e137c3edd8a01720b
kozen88 <93886188+kozen88@users.noreply.github.com>
2022-05-28 18:24:04 +0200

    Primo fix in conformità con checkstyle. (WordleGame)

--- src/main/java/it/uniba/app/WordleGame.java
+++ src/main/java/it/uniba/app/WordleGame.java
@@ -3,49 +3,41 @@ package it.uniba.app;
 /**
  * {@literal <<Entity>>} <br>
  * Classe che rappresenta una partita di Wordle.
- * Fornisce metodi per recuperare le impostazioni della partita, nonché la matrice dei tentativi.
+ * Fornisce metodi per recuperare le impostazioni della partita,
+ * nonché la matrice dei tentativi.
  */
-public class WordleGame
-{
+public class WordleGame {
     private final String secretWord;
     private final Board gameBoard;
 
     //costruttore
-    WordleGame(String secretWord)
-    {
+    WordleGame(String secretWord) {
         this(secretWord, 5, 6);
     }
 
     //costruttore con scelta parola e dimensioni del gioco
-    WordleGame(String secretWord, int column , int row )
-    {
+    WordleGame(String secretWord, int column, int row) {
         this.secretWord = secretWord;
         this.gameBoard = new Board(column, row);
     }
 
-    public String getSecretWord()
-    {
+    public String getSecretWord() {
         return secretWord;
     }
 
-    public Board getGameBoard()
-    {
+    public Board getGameBoard() {
         return gameBoard;
     }
 
-    public int getMaxGuesses()
-    {
+    public int getMaxGuesses() {
         return gameBoard.getRowsNumber();
     }
 
-    public int getWordLength()
-    {
+    public int getWordLength() {
         return gameBoard.getWordLength();
     }
 
-    public int getNumRemainingGuesses()
-    {
+    public int getNumRemainingGuesses() {
         return getMaxGuesses() - gameBoard.getNumFilledRows();
     }
-
 }
