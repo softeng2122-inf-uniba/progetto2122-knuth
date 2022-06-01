b11d81d40b8bd99e60da95dfba5815a816fae307
kozen88 <93886188+kozen88@users.noreply.github.com>
2022-06-01 20:47:14 +0200

    Quarto fix in conformità con checkstyle. (Parser)

--- src/main/java/it/uniba/app/Parser.java
+++ src/main/java/it/uniba/app/Parser.java
@@ -22,7 +22,8 @@ public class Parser {
         parserToken = null;
     }
 
-    public void feed(String inputLine) {
+    public void feed(final String inputLine) {
+
         this.input = inputLine.trim();
         tokens = tokenizeInput();
         Command command = extractCommand();
@@ -30,7 +31,7 @@ public class Parser {
         this.parserToken = new ParserToken(command, args);
 
         //Riconoscimento comando simile
-        if(command==Command.INVALID) {
+        if (command == Command.INVALID) {
             String wrongCommandToken = tokens[0].substring(1);
             parserToken.setCloseCommands(getCloseCommands(wrongCommandToken));
         }
@@ -55,25 +56,23 @@ public class Parser {
 
         // CASO 2: tentativo
         char firstChar = tokens[0].charAt(0);
-        if(firstChar != '/') {
+        if (firstChar != '/') {
             return Command.GUESS;
         } else { // CASO 3: altro comando valido
             String tokenCommand = tokens[0].substring(1);
 
-            for(Command c: Command.values()) {
-                if(tokenCommand.equalsIgnoreCase(c.toString()))
+            for (Command c: Command.values()) {
+                if (tokenCommand.equalsIgnoreCase(c.toString())) {
                     return c;
+                }
             }
-            //salvo solo il comando, i valori di enum non contengono "/"
         }
 
         // CASO 4: comando invalido
         return Command.INVALID;
-
     }
 
-    private String[] extractArgs(Command command)
-    {
+    private String[] extractArgs(final Command command) {
 
         if (tokens == null) {
             return null;
@@ -81,16 +80,17 @@ public class Parser {
 
         List<String> argsList = new LinkedList<>(Arrays.asList(tokens));
 
-        if(argsList.size() == 0)
+        if (argsList.size() == 0) {
             return null;
+        }
 
-        if(command != Command.GUESS) {
+        if (command != Command.GUESS) {
             argsList.remove(0);
         }
 
         int numArgsExpected = command.getNumArgs();
 
-        if(argsList.size() > numArgsExpected) {
+        if (argsList.size() > numArgsExpected) {
             argsList = argsList.subList(0, numArgsExpected);
         }
 
@@ -102,55 +102,61 @@ public class Parser {
         return argsArray;
     }
 
-    private int editDistance( String S, String T ) {
+    private int editDistance(String s, String t) {
         int i, j;
-        S = S.toUpperCase();
-        T = T.toUpperCase();
-        final int n = S.length(), m = T.length();
-        int[][]l = new int[n+1][m+1];
-        for ( i=0; i<n+1; i++ ) {
-            for ( j=0; j<m+1; j++ ) {
-                if ( i==0 || j==0 ) {
+        s = s.toUpperCase();
+        t = t.toUpperCase();
+
+        final int n = s.length(), m = t.length();
+        int[][]l = new int[n + 1][m + 1];
+
+        for (i = 0; i < (n + 1); i++) {
+            for (j = 0; j < (m + 1); j++) {
+                if (i == 0 || j == 0) {
                     l[i][j] = Math.max(i, j);
                 } else {
                     l[i][j] = min3(
-                            l[i-1][j] + 1,
-                            l[i][j-1] + 1,
-                            l[i-1][j-1] + (S.charAt(i-1) != T.charAt(j-1) ? 1 : 0) );
+                            l[i - 1][j] + 1,
+                            l[i][j - 1] + 1,
+                            l[i - 1][j - 1] + (s.charAt(i - 1)
+                                    != t.charAt(j - 1) ? 1 : 0));
                 }
             }
         }
         return l[n][m];
     }
 
-    private int min3( int i, int j, int k )
-    {
-        return Math.min(Math.min(i,j), k);
+    private int min3(final  int i, final int j, final int k) {
+
+        return Math.min(Math.min(i, j), k);
     }
 
-    private List<Command> getCloseCommands(String wrongCommandString){
+    private List<Command> getCloseCommands(final String wrongCommandString) {
+
         List<Command> closeCommands = null;
         int distance = 2;
         int editDist;
-        for (Command comparedCommand : Command.values())
-        {
+
+        for (Command comparedCommand : Command.values()) {
             String comparedString = comparedCommand.toString();
             if (comparedString == null) {
                 continue;
             }
+
             editDist = editDistance(comparedString, wrongCommandString);
-            if(editDist <= distance) {
+
+            if (editDist <= distance) {
                 if (closeCommands == null) {
                     closeCommands = new ArrayList<>();
                 }
                 closeCommands.add(comparedCommand);
             }
         }
+
         return closeCommands;
     }
+
     public ParserToken getParserToken() {
         return parserToken;
     }
-
-
 }
