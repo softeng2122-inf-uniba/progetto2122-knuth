192892b98070f4b34f3ab615665908e7ed36bfc4
npinto97 <ningo1797@gmail.com>
2022-06-04 18:00:11 +0200

    Aggiunta primi metodi di test per la classe GuessTest

--- src/test/java/it/uniba/app/wordle/domain/GuessTest.java
+++ src/test/java/it/uniba/app/wordle/domain/GuessTest.java
@@ -1,9 +1,10 @@
 package it.uniba.app.wordle.domain;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Un tentativo")
@@ -21,22 +22,92 @@ public class GuessTest {
     @DisplayName("quando si instanzia con new e una stringa come parametro")
     class createdWithPROVA {
 
+        @BeforeEach
+        void createNewGuess() {
+            g = new Guess("PROVA");
+        }
+
+        @Test
+        @DisplayName("contiene \"PROVA\"")
+        public void testGetWord(){
+            assertEquals("PROVA", g.getWord());
+        }
+
+        @Test
+        @DisplayName("inizia con 'P'")
+        public void testGetLetter() {
+            assertEquals('P',g.getLetter(0));
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = { -1, 5})
+        @DisplayName("lancia ArrayIndexOutOfBoundsException fuori dagli indici")
+        public void throwsExceptionWhenIndexOutOfBound(int i) {
+            assertThrows(ArrayIndexOutOfBoundsException.class, () -> g.getLetter(i));
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = { 0, 1, 2, 3, 4})
+        @DisplayName("contiene lettera NO_COLOR")
+        public void testGetColor(int i) {
+            assertEquals(Color.NO_COLOR, g.getColor(i));
+        }
+
+        @Nested
+        @DisplayName("quando P è colorata di verde")
+        class AfterSetColorGreen {
+
+            @Test
+            @DisplayName("risulta verde")
+            public void testSetColor() {
+                g.setColor(0, Color.GREEN);
+                assertEquals(Color.GREEN, g.getColor(0));
+            }
+
+        }
+
+        @Nested
+        @DisplayName("quando P è colorata di giallo")
+        class AfterSetColorYellow {
+
+            @Test
+            @DisplayName("risulta gialla")
+            public void testSetColor() {
+                g.setColor(0, Color.YELLOW);
+                assertEquals(Color.YELLOW, g.getColor(0));
+            }
+
+        }
+
+        @Nested
+        @DisplayName("quando P è colorata di grigio")
+        class AfterSetColorGrey {
+
+            @Test
+            @DisplayName("risulta grigio")
+            public void testSetColor() {
+                g.setColor(0, Color.GREY);
+                assertEquals(Color.GREY, g.getColor(0));
+            }
+
+        }
+
+
+
+
     }
 
-    // dfgdfg
-
+    @Nested
+    @DisplayName("quando è istanziato con new Guess(null)")
     class ConstructorTry {
         @Test
+        @DisplayName("lancia NullPointerException")
         void createNullGuess() {
             assertThrows(NullPointerException.class, () -> new Guess(null));
         }
     }
 
-    class
-    @BeforeEach
-    void createNewGuess() {
-            g = new Guess("PROVA");
-    }
+
 
 
 }
