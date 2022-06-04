05883a42b0a16a231c2a8fcb9b3da3e1664eecd0
npinto97 <ningo1797@gmail.com>
2022-06-04 20:02:53 +0200

    Effettuato testing della classe Board con coverage completo

--- src/test/java/it/uniba/app/wordle/domain/BoardTest.java
+++ src/test/java/it/uniba/app/wordle/domain/BoardTest.java
@@ -1,66 +1,70 @@
 package it.uniba.app.wordle.domain;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
-public class BoardTest {
+@DisplayName("Una board")
+class BoardTest {
 
     static Board board;
 
-    @BeforeAll
-    static void setUpAllBoard() {
-        board = new Board(5, 6);
-    }
+    @DisplayName("quando è istanziata con new Board(5, 6)")
+    static class WhenNew {
 
-    @Test
-    @DisplayName("Test del costruttore")
-    public void testBoard(){
-        assertEquals(5, board.getWordLength());
-        assertEquals(6, board.getRowsNumber());
-        assertEquals(0, board.getNumFilledRows());
-    }
-
-    @Test
-    public void testGetRowsNumber() {
-        assertEquals(6, board.getRowsNumber());
-    }
-
-    @Test
-    public void testGetWordLength() {
-        assertEquals(5, board.getWordLength());
-    }
-
-    @Test
-    public void testGetNumFilledRows() {
-        assertEquals(0, board.getNumFilledRows());
-    }
-
-    @Test
-    @DisplayName("Test di acceptNewGuess")
-    public void testAcceptNewGuess() {
-        Board board = new Board(5,6);
-        Guess guess = new Guess("nuova");
-        board.acceptNewGuess(guess);
-        assertEquals(guess,board.getGuess(board.getNumFilledRows()-1));
-    }
-
-    @Test
-    @DisplayName("Test di getGuess")
-    public void testGetGuess() {
-        Guess[] arrayGuess = {new Guess("nuova"),
-                              new Guess("treno"),
-                              new Guess("aiuto"),
-                              new Guess("piove"),
-                              new Guess("sauna")};
-
-        Board board = new Board(5,6);
-        for(int i=0; i < 5; i++){
-            board.acceptNewGuess(arrayGuess[i]);
+        @BeforeEach
+        void createNewBoard() {
+            board = new Board(5, 6);
         }
-        assertEquals(arrayGuess[0], board.getGuess(0));
+
+        @Test
+        @DisplayName("contiene 6 righe")
+        public void testGetRowsNumber() {
+            assertEquals(6, board.getRowsNumber());
+        }
+
+        @Test
+        @DisplayName("contiene 5 colonne")
+        public void testGetWordLength() {
+            assertEquals(5, board.getWordLength());
+        }
+
+        @Test
+        @DisplayName("è vuota")
+        public void testGetNumFilledRows() {
+            assertEquals(0, board.getNumFilledRows());
+        }
+
+        @Nested
+        @DisplayName("dopo l'inserimento del tentativo \"PROVA\"")
+        class AfterPushingAGuess {
+
+            Guess guess = new Guess("PROVA");
+            @BeforeEach
+            void pushAGuess() {
+                board.acceptNewGuess(guess);
+            }
+            //Se si inserisce una guess null
+
+            @Test
+            @DisplayName("la prima riga è piena")
+            void isNotEmpty() {
+                assertEquals(1, board.getNumFilledRows());
+            }
+
+            @Test
+            @DisplayName("la prima riga contiene il tentativo \"PROVA\"")
+            void returnPushedGuess() {
+
+                //Guess g = new Guess("PROVA");
+                //Per usare assertEquals è necessario l'override del metodo equals di Guess
+
+                assertSame(guess, board.getGuess(board.getNumFilledRows()-1));
+            }
+
+        }
+
     }
 
 }
