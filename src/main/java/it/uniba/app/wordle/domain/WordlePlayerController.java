df32a2c66d73ab3c08be702ce74f4b1285455723
npinto97 <ningo1797@gmail.com>
2022-06-05 18:16:16 +0200

    Modifica signature del metodo getnMaxGuesses()

--- src/main/java/it/uniba/app/wordle/domain/WordlePlayerController.java
+++ src/main/java/it/uniba/app/wordle/domain/WordlePlayerController.java
@@ -34,7 +34,7 @@ public final class WordlePlayerController implements PlayerController {
         }
 
         session.setCurrentGame(new WordleGame(session.getSecretWord(),
-                                              session.getnMaxGuesses(),
+                                              session.getNMaxGuesses(),
                                               session.getWordLength()));
     }
 
