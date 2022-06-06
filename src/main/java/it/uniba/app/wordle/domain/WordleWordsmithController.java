0c5c54828de2797d0ff64330e84a29c4c1d88a8f
kozen88 <93886188+kozen88@users.noreply.github.com>
2022-06-06 09:08:58 +0200

    Spostamento commenti javadoc.

--- src/main/java/it/uniba/app/wordle/domain/WordleWordsmithController.java
+++ src/main/java/it/uniba/app/wordle/domain/WordleWordsmithController.java
@@ -12,18 +12,6 @@ public final class WordleWordsmithController implements WordsmithController {
         session = playerController.getSession();
     }
 
-    /**
-     * Restituisce la parola segreta dalle impostazioni di gioco.<p></p>
-     * Nota: questo metodo, molto simile a {link Wordle#getGameSecretWord()},
-     * si differenzia da esso per l'origine del dato restituito
-     * (la differenza risiede nel recuperare l'impostazione attuale sulla
-     * parola segreta piuttosto che recuperare la parola segreta della
-     * partita già iniziata).<br>
-     * La scelta di tenere separati i due metodi è stata presa in quanto il
-     * nostro team ritiene che favorisca la modificabilità del codice.
-     * @throws WordleSettingException  se la parola segreta non è stata
-     * impostata
-     */
     public String getSecretWord() {
         if (!session.hasSecretWord()) {
             throw new WordleSettingException(
@@ -32,13 +20,6 @@ public final class WordleWordsmithController implements WordsmithController {
         return session.getSecretWord();
     }
 
-    /**
-     * Imposta la parola segreta.
-     * @param newWord   parola segreta da impostare
-     * @throws IllegalArgumentException se {@code newWord} non soddisfa
-     * la lunghezza prevista o contiene caratteri non validi
-     * @throws WordleGameException  se una partita è già in corso
-     */
     public void setSecretWord(final String newWord) {
         Objects.requireNonNull(newWord);
 
