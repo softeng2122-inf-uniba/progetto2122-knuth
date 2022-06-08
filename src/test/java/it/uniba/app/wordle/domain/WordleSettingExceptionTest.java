d8349bb07d4deecf8b71b2afee18d6eb77c51842
Emanuele Tanzi <e.tanzi2@studenti.uniba.it>
2022-06-08 10:51:18 +0200

    Applicazione suggerimenti Checkstyle nelle classi di test WordleGameExceptionTest e WordleSettingExceptionTest

--- src/test/java/it/uniba/app/wordle/domain/WordleSettingExceptionTest.java
+++ src/test/java/it/uniba/app/wordle/domain/WordleSettingExceptionTest.java
@@ -1,7 +1,5 @@
 package it.uniba.app.wordle.domain;
 
-import org.junit.jupiter.api.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
@@ -9,13 +7,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Una WordleSettingExceptionTest")
-public class WordleSettingExceptionTest {
+class WordleSettingExceptionTest {
 
     @ParameterizedTest
     @ValueSource(strings = {WordleSettingException.ABSENT_SECRET_WORD,
             "Stringa di prova"})
     @DisplayName("restituisce il messaggio corretto")
-    void testExceptionWithSpecialString(String message) {
+    void testExceptionWithSpecialString(final String message) {
         assertEquals(message, (new WordleSettingException(message))
                                                      .getMessage());
     }
